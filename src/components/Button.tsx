f11014b519acb6e3b97f4278265ea4f52dffa66b
Matheus Amaral <matheusamaral240797@gmail.com>
2022-10-31 23:28:56 -0300

    Publish front-end app

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,14 +5,16 @@ import { ReactNode } from 'react';
 export interface ButtonProps {
     children: ReactNode;
     asChild?: boolean;
+    className?: string;
 }
 
-export function Button({ children, asChild }: ButtonProps) {
+export function Button({ children, asChild, className }: ButtonProps) {
     const Comp = asChild ? Slot : 'button'  
 
     return(
       <Comp className={clsx(
-        'py-4 px-3 bg-cyan-500 rounded font-semibold text-black text-sm w-full transition-colors hover:bg-cyan-300',
+        'py-3 px-4 bg-cyan-500 rounded font-semibold text-black text-sm w-full transition-colors hover:bg-cyan-300',
+        className
       )}
       >
       {children}
